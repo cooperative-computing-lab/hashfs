6b20c036b6fe34f51ec7e60c94595a5ee79b3981
Kwan Ho Herman Tong <ktong1@cclvm00.virtual.crc.nd.edu>
2019-02-20 23:25:28 -0500

    mkfs_shell added

--- hashfs/delete.py
+++ hashfs/delete.py
@@ -3,9 +3,14 @@ import mkfs_core as mkfs
 import sys
 
 def DELETE(fs, dest_path, root_cksum):
-    nodes_traversed = list()
-    nodes_traversed, cksum = mkfs.get_node_by_path(fs, root_cksum, dest_path.split('/'), nodes_traversed)
+    nodes_traversed = list([('/', root_cksum)])
+    nodes_traversed, name, cksum = mkfs.get_node_by_path(fs, root_cksum, dest_path.split('/'), nodes_traversed)
 
-    print(mkfs.delete_file_bubble_up(fs, cksum, nodes_traversed))
+    if cksum is None:
+        print("Cannot delete {}".format(dest_path))
+        return "Unsuccessful"
 
-DELETE(sys.argv[1], sys.argv[2], sys.argv[3])
+    return mkfs.delete_node_bubble_up(fs, name, cksum, nodes_traversed)
+
+if __name__ == "__main__":
+    print("New head: {}".format(DELETE(sys.argv[1], sys.argv[2], sys.argv[3])))
